b36fab6c1e408d05d08b08c83ce24dd1ae8717ef
Felix S <felix@ftc-creative.de>
2016-07-01 04:05:06 +0200

    Updated VR with Global Illumination, Bump Shaders and UI, Image Effects and more

---
+++ VR_Adventure/Assets/Scripts/Startup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GvrViewer.Create();
+		Debug.Log("called GVR viewer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
