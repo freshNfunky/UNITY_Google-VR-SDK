49b8c997a1ab61eb024d2a3724e5eaffd0b872dc
felix schaller <inquiry@felixschaller.com>
2017-01-31 09:01:24 +0100

    Update

--- VR_Adventure/Assets/Scripts/Startup.cs
+++ VR_Adventure/Assets/Scripts/Startup.cs
@@ -8,8 +8,28 @@ public class Startup : MonoBehaviour {
 	public GameObject GvrReference;
 	private GvrHead gv;
 
-	void Start () {
 
+/// <summary>
+/// set screens to not fall asleep or dim, also selectively disable drift correction based on the device
+/// - currently only iOS disables drift correction
+/// Why disable drift correction?  Correction inherently relies on smoothing past data and extrapolating,
+/// but on a good device, it often cannot distinguish between head movement, and drift - so it will always
+/// make good data worse - on bad devices, it makes bad data much less bad, so the tradeoff may be worth it.
+///
+/// Further Extension: Double-check if GvrViewer is created and Reoprt in Log
+/// </summary>
+
+	void Start () {
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+		#if UNITY_IOS
+			Cardboard.SDK.AutoDriftCorrection = false;
+		#endif
+
+		#if UNITY_ANDROID
+
+		#endif
+		
 		//Doublecheck if GVR Viewer is created. React on Error Message in Console
 		gv = GvrReference.GetComponent<GvrHead>();
 		if (! gv.GvrCreated) {
@@ -20,9 +40,9 @@ public class Startup : MonoBehaviour {
 			Debug.Log("already created");
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
